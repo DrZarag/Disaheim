b6f289f06ba5aaa9af5884ff010d5195aa2d8388
Sammy <144029136+DrZarag@users.noreply.github.com>
2023-10-25 08:47:54 +0200

    Add project files.

---
+++ Amulet.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disaheim
+{
+    public class Amulet
+    {
+        public string ItemId;
+        public string Design;
+        public Level Quality = Level.medium;
+
+        public Amulet(string itemId)
+        {
+            this.ItemId = itemId;
+        }
+        public Amulet(string itemId, Level quality) : this(itemId)
+        {
+            this.ItemId = itemId;
+            this.Quality = quality;
+        }
+        public Amulet(string itemId, Level quality, string design) : this(itemId, quality)
+        {
+            this.ItemId = itemId;
+            this.Quality = quality;
+            this.Design = design;
+        }
+
+        public override string ToString()
+        {
+            return $"ItemId: {ItemId}, Quality: {Quality}, Design: {Design}";
+
+        }
+    }
+}
+
---
+++ Book.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disaheim
+{
+    public class Book
+    {
+        public string ItemId;
+        public string Title;
+        public double Price;
+
+        public Book (string itemId)
+        {
+            this.ItemId = itemId;
+        }
+        public Book (string itemId, string title): this(itemId)
+        {
+            this.ItemId = itemId;
+            this.Title = title;
+        }
+        public Book (string itemId, string title, double price): this(itemId, title)
+        {
+            this.ItemId = itemId;
+            this.Title = title;
+            this.Price = price;
+        }
+        public override string ToString()
+        {
+            return $"ItemId: {ItemId}, Title: {Title}, Price: {Price}";
+        }
+    }
+}
